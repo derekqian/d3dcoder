a8cc8a4947c0fa5bc04f16f05b8e767cf3be660e
Chao Fei <cfei@advde.com>
2022-07-01 21:46:30 +0800

    Upgrade d3d9c PartII Chapter 8 to VS2022
    Signed-off-by: Chao Fei <cfei@advde.com>

--- d3d9c/PartII/Chapter 8/MeshDemo/d3dUtil.h
+++ d3d9c/PartII/Chapter 8/MeshDemo/d3dUtil.h
@@ -19,10 +19,12 @@
 
 #include <d3d9.h>
 #include <d3dx9.h>
-#include <dxerr9.h>
+#include <dxerr.h>
 #include <string>
 #include <sstream>
 #include <vector>
+
+#pragma comment(lib, "legacy_stdio_definitions.lib")
 
 //===============================================================
 // Globals for convenient access.
